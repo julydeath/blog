aa9daa56dca6bc65495d11c21c13afed74515cdd
julydeath <manojkarjada.mk@gmail.com>
2025-03-28 00:03:01 +0530

    chore: generated types

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
-import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
